d15398bfd3215ab338812db07457f04880b5dae0
ritacc12 <rita20920@gmail.com>
2024-04-02 18:05:49 +0800

    import Countdown component

--- src/component/Activity.jsx
+++ src/component/Activity.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Countdown from "./Countdown.jsx";
 
 const Activity = () => {
   return (
@@ -11,7 +12,7 @@ const Activity = () => {
         <h3 className="font-semibold text-2xl text-[#455215]">
           ABOUT ACTIVITY{" "}
         </h3>
-
+        <Countdown />
         <h2 className="mx-auto px-8 py-6 font-bold text-wrap align-middle text-[#242d06] text-left">
           歡迎加入我們在2024年5月26日下午2:00至8:00在大安森林公園舉辦的野餐活動！這將是一場精彩的市集式野餐，您可以與家人和朋友一同享受美好時光。活動將在公園中的指定區域舉行，我們將提供各種美味的野餐食品和飲料，包括沙拉、三明治、水果、飲料等。此外，我們還準備了多種娛樂活動，如運動、音樂表演和舞蹈表演，保證讓您度過一個充滿活力和歡樂的下午。歡迎大家攜帶野餐墊或毯子，與我們一同享受美好的天氣和自然景觀。我們期待與您相見，一起共度愉快時光！
         </h2>
