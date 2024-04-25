fd4787f2acfa4157e017ceacb79ce1ff357389d8
ritacc12 <rita20920@gmail.com>
2024-04-26 00:57:37 +0800

    import GoogleLogIn component and set the validation rule of form

--- src/component/Registration.jsx
+++ src/component/Registration.jsx
@@ -1,33 +1,45 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import GoogleLogIn from "./GoogleLogIn.jsx";
 
 const Registration = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    profileState: "",
     activity: "",
   });
 
   const [errors, setErrors] = useState({});
   const [submitted, setSubmitted] = useState(false);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+  // For Radio button only can choose one choice
 
+  const handleActivityChange = (e) => {
     setFormData({
       ...formData,
-      [name]: value,
+      activity: e.target.value,
     });
   };
 
-  // For Radio button only can choose one choice
-  const [selectedValue, setSelectedValue] = useState("");
-  const handleRadioChange = (value) => {
-    setSelectedValue(value);
-
+  useEffect(() => {
     setFormData({
       ...formData,
-      activity: value,
+      activity: "",
+    });
+  }, [submitted]);
+
+  //處理登入成功的狀態，更新服組建狀態
+  const handleLoginSuccess = (user) => {
+    setFormData({
+      ...formData,
+      profileState: user, // 將用戶資訊更新
+    });
+  };
+
+  //處理登出的狀態，更新服組建狀態
+  const handleLogout = () => {
+    setFormData({
+      ...formData,
+      profileState: "",
     });
   };
 
@@ -35,22 +47,16 @@ const Registration = () => {
     let isValid = true;
     const newErrors = {};
 
-    // Validate Name
-    if (!formData.name) {
-      newErrors.name = "Name is required";
-      isValid = false;
-    }
-    // Validate email
-    if (!formData.email) {
-      newErrors.email = "Email is required";
-      isValid = false;
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)
-    ) {
-      newErrors.email = "Email type error";
+    if (!formData.activity) {
+      newErrors.activity = "Activity is required";
       isValid = false;
     }
 
+    //  Validate logIn
+    if (!formData.profileState) {
+      newErrors.profileState = "Please log in";
+      isValid = false;
+    }
     setErrors(newErrors);
     return isValid;
   };
@@ -59,10 +65,8 @@ const Registration = () => {
     e.preventDefault();
 
     if (validateForm()) {
-      console.log("Form data:", formData);
-      setSubmitted(true); // Set a submitted flag
+      setSubmitted(true);
     } else {
-      // Form is not valid, display error messages
       setSubmitted(false);
     }
   };
@@ -77,17 +81,19 @@ const Registration = () => {
         ACTIVITY REGISTRATION
       </h3>
       <p className="text-[#434a1d] mt-3 font-bold"> *一人限報名一個場次</p>
-      <div className=" flex flex-col justify-center mx-auto mt-8">
+      <div className="mx-auto mt-4">
         {submitted ? (
           <>
-            <div className="success-message font-extrabold text-[#23280b] text-4xl">
-              Successful
-              <p className="font-bold pt-2">已成功報名</p>
+            <div className="mt-20">
+              <div className="success-message font-extrabold text-[#23280b] text-4xl">
+                Successful
+                <p className="font-bold pt-2">已成功報名</p>
+              </div>
+              <p className="text-[#50571d] mt-2 font-bold">
+                If you need to register for other activities, please refresh
+                your browser
+              </p>
             </div>
-            <p className="text-[#50571d] mt-4 font-bold">
-              If you need to register for other activities, please refresh your
-              browser
-            </p>
           </>
         ) : (
           <form
@@ -95,63 +101,23 @@ const Registration = () => {
             className="mx-auto flex flex-col justify-center"
           >
             <div>
-              <label className="input input-bordered flex items-center gap-2 bg-zinc-300">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  className="w-4 h-4 opacity-70"
-                >
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
-                <input
-                  type="text"
-                  name="name"
-                  className="grow"
-                  placeholder="Name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                />
-              </label>
-              {errors.name && (
-                <div className="error text-red-500 italic font-medium pt-2">
-                  {errors.name}
-                </div>
-              )}
+              <GoogleLogIn
+                setProfileState={setFormData}
+                onLoginSuccess={handleLoginSuccess}
+                onLogout={handleLogout}
+              />
             </div>
-
-            <div className="my-4">
-              <label className="input input-bordered flex items-center gap-2 bg-zinc-300 ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  className="w-4 h-4 opacity-70"
-                >
-                  <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                  <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
-                <input
-                  type="text"
-                  name="email"
-                  className="grow"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
-              </label>
-              {errors.email && (
-                <div className="error text-red-500 italic font-medium pt-2">
-                  {errors.email}
-                </div>
-              )}
-            </div>
-
+            {errors.profileState && (
+              <div className="error text-red-500 font-semibold mt-4 outline-double outline-3 outline-offset-2">
+                {errors.profileState}
+              </div>
+            )}
             <div className="form-control my-6">
               <div className="text-[#23280b]">
-                <legend className="text-left font-semibold mb-4 text-lg">
+                <legend className="text-left font-semibold  text-lg">
                   報名活動：
                 </legend>
+
                 <div>
                   <div className="cursor-pointer flex justify-start mt-2">
                     <input
@@ -160,8 +126,8 @@ const Registration = () => {
                       id="SoothYoga"
                       name="SoothYoga"
                       value="SoothYoga"
-                      checked={selectedValue === "SoothYoga"}
-                      onChange={() => handleRadioChange("SoothYoga")}
+                      checked={formData.activity === "SoothYoga"}
+                      onChange={handleActivityChange}
                     />
                     <label
                       htmlFor="舒緩瑜珈"
@@ -177,9 +143,9 @@ const Registration = () => {
                       className="radio radio-warning"
                       id="HathaYoga"
                       name="HathaYoga"
-                      value={formData.activity}
-                      checked={selectedValue === "HathaYoga"}
-                      onChange={() => handleRadioChange("HathaYoga")}
+                      value="HathaYoga"
+                      checked={formData.activity === "HathaYoga"}
+                      onChange={handleActivityChange}
                     />
                     <label
                       htmlFor="哈達瑜珈"
@@ -195,9 +161,9 @@ const Registration = () => {
                       className="radio radio-warning"
                       id="AerobicBoxing"
                       name="AerobicBoxing"
-                      value={formData.activity}
-                      checked={selectedValue === "AerobicBoxing"}
-                      onChange={() => handleRadioChange("AerobicBoxing")}
+                      value="AerobicBoxing"
+                      checked={formData.activity === "AerobicBoxing"}
+                      onChange={handleActivityChange}
                     />
                     <label
                       htmlFor="有氧拳擊"
@@ -208,10 +174,14 @@ const Registration = () => {
                   </div>
                 </div>
               </div>
+              {errors.activity && (
+                <div className="error mt-4 text-red-500  font-semibold outline-double outline-3 outline-offset-2">
+                  {errors.activity}
+                </div>
+              )}
             </div>
-
             <button
-              className="rounded-full bg-[#f5a342] cursor-pointer w-72 shadow-lg font-semibold my-4 text-[#23280b]"
+              className="rounded-full bg-[#f5a342] cursor-pointer w-72 shadow-lg font-bold text-[#23280b] hover:ring-2 ring-orange-200 mt-2"
               type="submit"
             >
               Submit
