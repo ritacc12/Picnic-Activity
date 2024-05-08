a4cfa141d1ff4f2967f1e85ed79c4bd254c2a826
ritacc12 <rita20920@gmail.com>
2024-05-08 16:38:57 +0800

    adjust the activity description paragraphs

--- src/component/Activity.jsx
+++ src/component/Activity.jsx
@@ -13,9 +13,15 @@ const Activity = () => {
           ABOUT ACTIVITY{" "}
         </h3>
         <Countdown />
-        <h2 className="mx-auto px-8 py-6 font-bold text-wrap align-middle text-[#242d06] text-left">
-          歡迎加入我們在2024年5月26日下午2:00至8:00在大安森林公園舉辦的野餐活動！這將是一場精彩的市集式野餐，您可以與家人和朋友一同享受美好時光。活動將在公園中的指定區域舉行，我們將提供各種美味的野餐食品和飲料，包括沙拉、三明治、水果、飲料等。此外，我們還準備了多種娛樂活動，如運動、音樂表演和舞蹈表演，保證讓您度過一個充滿活力和歡樂的下午。歡迎大家攜帶野餐墊或毯子，與我們一同享受美好的天氣和自然景觀。我們期待與您相見，一起共度愉快時光！
-        </h2>
+        <p className="mx-auto px-8 py-8 font-bold align-middle text-[#242d06] text-center text-xl">
+          歡迎加入我們在2024年5月26日下午2:00至8:00在大安森林公園舉辦的野餐活動！
+          <br></br>
+          活動將在公園中的指定區域舉行，我們將提供各種美味的美味佳餚，包括沙拉、三明治、水果、飲料等。
+          <br></br>
+          此外，我們還準備了多種娛樂活動，如運動、音樂表演和舞蹈表演，保證讓您度過一個充滿活力和歡樂的下午。
+          <br></br>
+          歡迎大家攜帶野餐墊或毯子，與我們一同享受美好的午後。我們期待與您相見，一起共度愉快時光！
+        </p>
         <div className="flex flex-row mx-auto justify-around flex-wrap mt-4">
           <div className="flex flex-col items-center px-2 mb-3">
             <img
